c878dd96df0adcf772c932968847efaf511530a8
Shubham Mishra <codermishra53@gmail.com>
2025-08-29 17:40:22 +0530

    set the error

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 
 // Replace with your Firebase service account JSON
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('./config/google-services.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
