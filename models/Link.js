7f498a0d576b3b4ebf9fb74e2a7fdecda11c5893
Shubham Mishra <142476256+Shubham53Mishra@users.noreply.github.com>
2025-08-30 19:52:16 +0530

    Update Link.js

--- models/Link.js
+++ models/Link.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const linkSchema = new mongoose.Schema({
   short: { type: String, required: true, unique: true },
   target: { type: String, required: true },
-  senderUid: { type: String, required: true },
-  receiverUid: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
