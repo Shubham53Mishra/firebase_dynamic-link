0ac80ffe283d7c518ef92f273de867673ef541db
Shubham Mishra <142476256+Shubham53Mishra@users.noreply.github.com>
2025-09-01 17:02:58 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,9 @@
-const firebase = require('firebase/app');
-require('firebase/auth'); // If you need authentication
-require('firebase/firestore'); // If you need Firestore
-require('firebase/database'); // If you need Realtime Database
-require('firebase/storage');  // If you need Storage
+const admin = require('firebase-admin');
+const serviceAccount = require('./config/serviceAccountKey.json');
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBisoTANGdB8to8jWonfKFSwBYiWpB9v1Q",
-  authDomain: "dailynest-e2487.firebaseapp.com",
-  databaseURL: "https://dailynest-e2487-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "dailynest-e2487",
-  storageBucket: "dailynest-e2487.firebasestorage.app",
-  messagingSenderId: "435420807936",
-  appId: "1:435420807936:android:ea967b1d797980311d69e5"
-};
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+  // databaseURL: "https://lifecycle-management-app-459ea.firebaseio.com" // (optional, for Realtime DB/Firestore)
+});
 
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-module.exports = firebase;
+module.exports = admin;
