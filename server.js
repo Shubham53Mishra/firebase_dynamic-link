10c110acb67974ebbaab9ecdcf8d94ab842e82e8
Shubham Mishra <142476256+Shubham53Mishra@users.noreply.github.com>
2025-09-01 18:10:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+
 const linkRoutes = require('./routes/link');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 
 
 app.use('/api', linkRoutes);
